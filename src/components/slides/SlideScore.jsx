989f89df1bdb0ae32481a9ff40401c8f68d3779f
Mykhailo <sakurai.pnwz@gmail.com>
2026-08-26 15:29:35 +0300

    wip: 2nd slide

--- src/components/slides/SlideScore.jsx
+++ src/components/slides/SlideScore.jsx
@@ -1,3 +1,36 @@
-export default function SlideScore() {
+import { TOTAL_ETC, TOTAL_ME, TOTAL_MERGE, TOTAL_SHE, TOTAL_TITLE } from "../../constants/strings"
+import { Button, Text, Title } from "../ui/Typography"
+import metrics from '../../constants/metrics.json';
 
+const mapMedia = (m) => ({
+    photos: m.photo,
+    videos: m.video_file,
+    voices: m.voice_message,
+    roundVideo: m.video_message,
+});
+
+export default function SlideScore() {
+    const { messages_me, messages_her, media_me, media_her, media_total } = metrics.score;
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center pb-8">
+            <Title className="text-2xl pb-12">{TOTAL_TITLE}</Title>
+
+            <div className="flex gap-32">
+                <div className="flex flex-col">
+                    <Text className="text-8xl">{messages_me}</Text>
+                    <Text className="text-4xl">{TOTAL_ME}</Text>
+                    <Text className="text-xl">{TOTAL_ETC(mapMedia(media_me))}</Text>
+                </div>
+
+                <div className="w-px h-32 bg-text/20" />
+
+                <div className="flex flex-col">
+                    <Text className="text-8xl">{messages_her}</Text>
+                    <Text className="text-4xl">{TOTAL_SHE}</Text>
+                    <Text className="text-xl">{TOTAL_ETC(mapMedia(media_her))}</Text>
+                </div>
+            </div>
+            <Button className="mt-16">{TOTAL_MERGE}</Button>
+        </div>
+    )
 }
--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,8 +1,16 @@
-// 1-й слайд - приветствие
-export const REPLY = `Reply // 2026`
+// 1-й слайд - интро
+export const REPLY = `Reply // 2026`;
 export const GREETING = `Привет!
 Позволь тебе сразу рассказать, что такое Reply и зачем я его скинул тебе. 😋
 Reply — это наши итоги года (а-ля Spotify Wrapped, YouTube Recap, и всё в этом духе). Ты узнаешь о некоторых интересных вещах из наших отношений. Надеюсь, ты поняла.
-Ну что, интересно? 😎`
-export const LETS_GO = `Вперёд!`
+Ну что, интересно? 😎`;
+export const LETS_GO = `Вперёд!`;
 
+// 2-й слайд - всего сообщений
+export const TOTAL_TITLE = `Так, а сколько мы вообще общались?`;
+export const TOTAL_BOTTOMTEXT = `Неплохая цифра. Было бы круто, если бы такую тебе скинули на карту, правда?`;
+export const TOTAL_ME = `сообщений написал Миша`;
+export const TOTAL_SHE = `сообщений написала Ксюша`;
+export const TOTAL_ETC = ({ photos, videos, voices, roundVideo }) =>
+    `(из них ${photos} фоток, ${videos} видео, ${voices} голосовых и ${roundVideo} кружка.)`;
+export const TOTAL_MERGE = `Слить воедино!`;
